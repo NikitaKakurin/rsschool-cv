41c37e66763cb6a72972af2700f37851fe9cb0a2
NikitaKakurin <neyasut87@rambler.ru>
2022-01-03 22:33:30 +0300

    feat: add adaptive and burger menu

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 "use strict"
-
 hljs.highlightAll();
+
+const checkbox =document.getElementById("toggle-menu");
+const menu = document.querySelector(".menu__list");
+checkbox.addEventListener("change",menuToDisplay)
+function menuToDisplay(e){
+    if(checkbox.checked){
+        menu.style.height = "600px"
+        menu.style.display = "flex";
+    }else{
+        menu.style.height = "0px"
+        menu.style.display = "none";
+    }
+}
+// window.onresize = function(event){
+//     if(document.documentElement.clientWidth >769){
+//         menu.style.display = "flex";
+//         menu.style.marginBottom ="20px";
+//         menu.style.height ="20px";
+//     };
+//     if(document.documentElement.clientWidth<=768){
+//         menu.style.display = "none";
+//     };
+// }
