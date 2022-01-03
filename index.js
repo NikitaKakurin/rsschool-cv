cef2cd408e41ec639c14bafe1367aaa8bf989fd0
NikitaKakurin <neyasut87@rambler.ru>
2022-01-03 23:14:52 +0300

    frat: fix burger menu

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ hljs.highlightAll();
 
 const checkbox =document.getElementById("toggle-menu");
 const menu = document.querySelector(".menu__list");
-checkbox.addEventListener("change",menuToDisplay)
+const menuButton = document.querySelector(".menu-button");
+checkbox.addEventListener("change",menuToDisplay);
 function menuToDisplay(e){
+    console.log("work");
     if(checkbox.checked){
-        menu.style.height = "600px"
+        menu.style.height = "300px"
         menu.style.display = "flex";
     }else{
         menu.style.height = "0px"
         menu.style.display = "none";
     }
 }
-// window.onresize = function(event){
-//     if(document.documentElement.clientWidth >769){
-//         menu.style.display = "flex";
-//         menu.style.marginBottom ="20px";
-//         menu.style.height ="20px";
-//     };
-//     if(document.documentElement.clientWidth<=768){
-//         menu.style.display = "none";
-//     };
-// }
+window.onresize = function(event){
+    if(document.documentElement.clientWidth >769){
+        menu.style.display = "flex";
+        menu.style.marginBottom ="20px";
+        menu.style.height ="20px";
+    };
+    if(document.documentElement.clientWidth<=768){
+        menu.style.display = "none";
+    };
+}
+
